164b3bc576eef9eef83833f6da133a93aae424f0
DEZester <aromudez@gmail.com>
2023-08-12 15:54:56 +0300

    First part of Feed page

---
+++ app/api/prompt/route.js
@@ -0,0 +1,14 @@
+import {connectToDB} from '@utils/database'
+import Prompt from "@models/prompt";
+
+export const GET = async (request) => {
+  try {
+    await connectToDB()//connect to DB
+
+    const prompts = await Prompt.find({}).populate('creator')//find posts
+
+    return new Response(JSON.stringify(prompts), {status: 200})//return posts
+  } catch (error) {
+    return new Response("Failed to fetch all prompts", {status: 500})
+  }
+}
